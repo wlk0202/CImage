354ec3043a761a4259511ddc2330a337acfef3bc
wlk0202 <54826094+wlk0202@users.noreply.github.com>
2019-10-29 15:14:07 +0900

    Add files via upload

---
+++ image.cpp
@@ -0,0 +1,30 @@
+
+#include<afxwin.h>
+class CMainFrame :public CFrameWnd {
+public:
+	CMainFrame() { Create(NULL, L"Hi"); }
+
+	DECLARE_MESSAGE_MAP()
+	afx_msg void OnPaint();
+};
+class CMyApp : public CWinApp {
+	BOOL InitInstance() {
+		CMainFrame* Frm = new CMainFrame();
+		m_pMainWnd = Frm;
+		Frm->ShowWindow(1);
+		return 1;
+	}
+};
+CMyApp theApp; BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+#include <atlimage.h>//CImage img;를 사용하기 위해
+void CMainFrame::OnPaint()
+{
+	CImage img;
+	CPaintDC dc(this);
+	img.Load(L"parrot.png");//""안은 파일명
+	img.Draw(dc, 0, 0);
+	
+}
